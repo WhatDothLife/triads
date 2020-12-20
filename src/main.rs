97f563afe79147b7fb550fe98b01aefbdbe4a486
Michael Wernthaler <michael.wernthaler@posteo.de>
2020-12-20 23:38:28 +0100

    Changed example in main

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() {
     // h.insert_vertex(0);
     // h.insert_vertex(1);
     // h.insert_vertex(2);
+    // h.insert_vertex(3);
 
     // h.insert_edge(&0, &1);
     // h.insert_edge(&1, &2);
-    // h.insert_edge(&1, &0);
+    // h.insert_edge(&2, &3);
 
     // let mut g = AdjacencyList::<u32>::new();
 
@@ -16,23 +17,27 @@ fn main() {
     // g.insert_vertex(20);
     // g.insert_vertex(30);
     // g.insert_vertex(40);
+    // g.insert_vertex(50);
 
     // g.insert_edge(&10, &20);
-    // g.insert_edge(&20, &30);
-    // g.insert_edge(&20, &40);
+    // g.insert_edge(&30, &20);
+    // g.insert_edge(&40, &30);
+    // g.insert_edge(&50, &40);
 
-    // let l = arc_consistency(&g, &h);
+    // let l = ac_3(&h, &g);
     // println!("{:?}", &l);
 
-    let triad = Triad::from_vecs(
-        vec![true, true],
-        vec![false],
-        vec![true, false, false, false],
-    );
+    // let triad = Triad::from_vecs(
+    //     vec![true, true],
+    //     vec![false],
+    //     vec![true, false, false, false],
+    // );
 
-    let triad2 = Triad::from_strings("11", "0", "1001");
+    let triad = Triad::from_strings("1", "00", "0111");
 
-    let list = triad2.adjacency_list();
+    let list = triad.adjacency_list();
     let l2 = arc_consistency(&list, &list);
+    let l3 = ac_3(&list, &list);
     println!("{:?}", &l2);
+    println!("{:?}", &l3);
 }
