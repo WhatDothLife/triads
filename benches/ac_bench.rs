a4c9144bc5764c721b5ac1a6dbb1fc23be147366
Michael Wernthaler <michael.wernthaler@posteo.de>
2021-01-11 00:17:42 +0100

    Updated benchmarks

--- benches/ac_bench.rs
+++ benches/ac_bench.rs
@@ -3,9 +3,9 @@ use std::collections::HashMap;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use triads::{arc_consistency::ac3, arc_consistency::arc_consistency, core_triads::Triad};
 
-fn ac_benchmark(c: &mut Criterion) {
+fn ac3_benchmark(c: &mut Criterion) {
     let mut triad = Triad::new();
-    triad.add_arm("01011010111010011010001001011110010110000000011");
+    triad.add_arm("01101011101001");
     // triad.add_arm("010110101110100110100");
     let list = triad.adjacency_list();
 
@@ -14,7 +14,18 @@ fn ac_benchmark(c: &mut Criterion) {
     });
 }
 
-fn ac3_benchmark(c: &mut Criterion) {
+fn ac3_benchmark_double(c: &mut Criterion) {
+    let mut triad = Triad::new();
+    triad.add_arm("0110101110100101001000101101");
+    // triad.add_arm("010110101110100110100");
+    let list = triad.adjacency_list();
+
+    c.bench_function("ac_3_double", |b| {
+        b.iter(|| ac3(black_box(&list), black_box(&list)))
+    });
+}
+
+fn ac_benchmark(c: &mut Criterion) {
     let mut triad = Triad::new();
     triad.add_arm("01011010111010011010001001011110010110000000011");
     // triad.add_arm("010110101110100110100");
@@ -25,5 +36,5 @@ fn ac3_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, ac_benchmark, ac3_benchmark);
+criterion_group!(benches, ac3_benchmark, ac3_benchmark_double);
 criterion_main!(benches);
