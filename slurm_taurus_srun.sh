60c5e3da7373233253f92a13202e6ede5718706c
Michael Wernthaler <michael.wernthaler@posteo.de>
2021-06-28 16:04:50 +0200

    Added new data

--- slurm_taurus_srun.sh
+++ slurm_taurus_srun.sh
@@ -6,13 +6,13 @@
 #SBATCH --cpus-per-task=24  # number of processor cores (i.e. threads)
 #SBATCH --partition=haswell
 #SBATCH --mem-per-cpu=2048M   # memory per CPU core
-#SBATCH --output=/scratch/ws/0/s8179597-triads/com2526.output
-#SBATCH -J "com2526"   # job name
+#SBATCH --output=/scratch/ws/0/s8179597-triads/majs.output
+#SBATCH -J "majs"   # job name
 #SBATCH -A p_triads
 
 srun ./target/release/tripolys \
 	--data /scratch/ws/0/s8179597-triads/data \
-	--nodes 25-26 \
-	--polymorphism commutative
+	--triad 0101000,011011,1 \
+	--polymorphism majority
 
 scontrol show job "$SLURM_JOB_ID"
